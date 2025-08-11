ac15566e2d37efa8b9928852a5e4286cf12be194
goncalohenrique <gonriq01@gmail.com>
2025-08-10 23:31:36 -0300

    feito: Services dos Jogadores, comecei controller dos jogadores a fazer: Continuar os controllers de times e jogadores.

--- src/main/java/controller/JogadorController.java
+++ src/main/java/controller/JogadorController.java
@@ -5,6 +5,7 @@ import dto.JogadorResponseDTO;
 import jakarta.validation.Valid;
 import mapper.JogadorMapper;
 import model.Jogador;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,11 +22,17 @@ public class JogadorController {
     private JogadoresServices jogadoresServices;
     private JogadorMapper jogadorMapper;
 
+    @Autowired
+    public JogadorController(JogadoresServices jogadoresServices, JogadorMapper jogadorMapper) {
+        this.jogadoresServices = jogadoresServices;
+        this.jogadorMapper = jogadorMapper;
+    }
+
     @PostMapping
-    public ResponseEntity<Jogador> cadastrar_Jogador(@RequestBody @Valid JogadorRequestDTO jogdto)
-    {
+    public ResponseEntity<JogadorResponseDTO> cadastrar_Jogador(@RequestBody @Valid JogadorRequestDTO jogdto) {
         Jogador jogador_salvo = jogadoresServices.cadastrar_jogador(jogdto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(jogador_salvo);
+        JogadorResponseDTO dto = jogadorMapper.jogador_para_dto(jogador_salvo);
+        return ResponseEntity.status(HttpStatus.CREATED).body(dto);
     }
 
     @GetMapping
@@ -34,6 +41,7 @@ public class JogadorController {
         return ResponseEntity.ok(jogadoresServices.buscar_todos_jogadores());
     }
 
+    @GetMapping("/{id_jogador}")
     public  ResponseEntity<JogadorResponseDTO> buscar_porId(@PathVariable Long id_jogador)
     {
         Optional<JogadorResponseDTO> jogad_dto_optional = jogadoresServices.buscar_jogad_porId(id_jogador);
