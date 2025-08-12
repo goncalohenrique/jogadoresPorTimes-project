d38264d3ae1aef2552f286665cf461bd8ad19a0c
goncalohenrique <gonriq01@gmail.com>
2025-08-11 22:04:30 -0300

    feito: CRUD concluído, camadas prinicipais concluídas, API Funcionando! a fazer: fazer validações, testes unitários, conexão com docker

--- src/main/java/com/goncalo/jogadores/dto/JogadorRequestDTO.java
+++ src/main/java/com/goncalo/jogadores/dto/JogadorRequestDTO.java
@@ -15,12 +15,12 @@ import lombok.Setter;
 public class JogadorRequestDTO {
 
     @NotNull(message = "Informe o número de gols!")
-    @Min(value = 0, message = "Número de gols não pode ser negativo")
+    @Min(value = 0, message = "Número de gols não pode ser negativo!")
     private Integer gols;
     @NotBlank(message = "Nome do jogador é obrigatório!")
     private String nome;
     @NotNull(message = "Sálario do jogador é onrigatório!")
-    @Min(value = 0, message = "Salário não pode ser negativo")
+    @Min(value = 0, message = "Salário não pode ser negativo!")
     private Long salario;
     @NotNull(message = "O id do time é obrigatório!")
     private Long idTime;
